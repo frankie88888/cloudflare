2439574f80103c780afe1838b118952f152ef214
frankieky888 <frankieky888@gmail.com>
2023-11-13 17:04:26 +0800

    add git commit into code.js

--- src/Code.js
+++ src/Code.js
@@ -10,10 +10,29 @@ export default function Code() {
                 <tr><td>React<br /></td></tr>
                 <tr><td>Create react 框架 : npx create-react-app &lt;name&gt;</td></tr>
                     <tr><td>test application : npm start</td></tr>
+                    <tr><td></td></tr>
+                    <tr><td></td></tr>
+                    <tr><td></td></tr>
+                    <tr><td></td></tr>
+                    <tr><td></td></tr>
                     <tr><td><br/></td></tr>
                     <tr><td>Node.js</td></tr>
+                    <tr><td></td></tr>
+                    <tr><td></td></tr>
+                    <tr><td></td></tr>
+                    <tr><td></td></tr>
+                    <tr><td></td></tr>
                     <tr><td><br/></td></tr>
                     <tr><td>Git</td></tr>
+                    <tr><td>git init</td></tr>
+                    <tr><td>git add .</td></tr>
+                    <tr><td>git commit -m "comment"</td></tr>
+                    <tr><td>git log</td></tr>
+                    <tr><td>git status</td></tr>
+                    <tr><td>.gitignore</td></tr>
+                    <tr><td>git checkout hash</td></tr>
+                    <tr><td>git revert hash</td></tr>
+                    <tr><td>git reset --hard hash</td></tr>
                     <tr><td><br/></td></tr>
                 </tbody>
             </table>
