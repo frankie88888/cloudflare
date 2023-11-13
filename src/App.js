c590e959a661f6b287bc007f69450fe8d8004afc
frankieky888 <frankieky888@gmail.com>
2023-11-13 16:05:17 +0800

    水果店app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Checkout from './Checkout';
+import ProductDetail from './ProductDetail';
+import ProductList from './ProductList';
+import {CartContext} from './CartContext'
+import { useState } from 'react';
 
 function App() {
+
+	const [cartItems, setCartItems] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+		<CartContext.Provider value={{cartItems,setCartItems}}>
+
+			<nav>
+				<Link to="/">首頁</Link> 
+				<Link to="/checkout">購物車</Link>
+			</nav>
+
+			<Routes>
+				<Route path="/" element={<ProductList/>} />
+				<Route path="/checkout" element={<Checkout/>} />
+
+				<Route path="/product" element={<ProductDetail/>}>
+					<Route path=":id" element={<ProductDetail/>} />
+				</Route>
+
+				<Route path="*" element={<p>找不到頁面</p>}/>
+			</Routes>
+
+		</CartContext.Provider>
+
+	</BrowserRouter>
   );
 }
 
